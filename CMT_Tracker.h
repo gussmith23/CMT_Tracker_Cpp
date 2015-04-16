7cbe2b9c13c1c759172bcd3083e8f9c22fab384b
Gus Smith <hfs5022@psu.edu>
2015-04-16 09:15:54 -0400

    First commit.

---
+++ CMT_Tracker.h
@@ -0,0 +1,10 @@
+class CMT_Tracker
+{
+private:
+	bool mSurfEnabled; // False by default
+	bool mFullSURFEnabled; // False by default
+	
+	int mDeltaX;
+	int mDeltaY;
+	float mDeltaPercent; // 0.2F by default
+};
